17c2813e48b164ab60d297f0500509ff441d884d
K-DOT <aleximeky@gmail.com>
2016-01-07 21:21:43 +0400

    Fix

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from app import app
 from flask import render_template
 from app.forms import MainForm
-from multiprocessing.pool import ThreadPool
 from lastfm import LastFMError, get_track_genre, get_track_count
 
 @app.route('/', methods=['POST', 'GET'])
@@ -9,7 +8,6 @@ def index():
     form = MainForm()
     if form.validate_on_submit():
         error = None
-        pool = ThreadPool(processes=1)
         try:
             count = get_track_count(form.username.data, form.limit.data, form.period.data)
         except LastFMError as e:
@@ -17,6 +15,6 @@ def index():
             res = None
         else:
             print(count)
-            res = pool.apply_async(get_track_genre, (count,)).get()
+            res = get_track_genre(count)
         return render_template('index.html', form=form, result=res, error=error)
     return render_template('index.html', form=form)
