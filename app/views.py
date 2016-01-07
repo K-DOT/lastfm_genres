4f6226607241a06e39c202bdfd593afee96a001c
K-DOT <aleximeky@gmail.com>
2016-01-07 19:25:34 +0400

    Working version

--- app/views.py
+++ app/views.py
@@ -2,15 +2,21 @@ from app import app
 from flask import render_template
 from app.forms import MainForm
 from multiprocessing.pool import ThreadPool
-from lastfm import get_track_genre, get_track_count
+from lastfm import LastFMError, get_track_genre, get_track_count
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     form = MainForm()
     if form.validate_on_submit():
+        error = None
         pool = ThreadPool(processes=1)
-        count = get_track_count(form.username.data,form.limit.data,form.period.data)
-        print(count)
-        res = pool.apply_async(get_track_genre, (count,)).get()
-        return render_template('index.html', form=form, result=res)
+        try:
+            count = get_track_count(form.username.data, form.limit.data, form.period.data)
+        except LastFMError as e:
+            error = e
+            res = None
+        else:
+            print(count)
+            res = pool.apply_async(get_track_genre, (count,)).get()
+        return render_template('index.html', form=form, result=res, error=error)
     return render_template('index.html', form=form)
--- lastfm.py
+++ lastfm.py
@@ -5,15 +5,25 @@ import xml.etree.ElementTree as ET
 
 API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
+class LastFMError(Exception):
+    pass
+        
+def errors(xml):
+    root = ET.fromstring(xml)
+    for i in root.iter('error'):
+        return i.text
+    return None    
 
 def make_request(params):
     url_params = urllib.parse.urlencode(params)
     url = 'http://ws.audioscrobbler.com/2.0/?%s' % url_params
     print(url)
-    return urllib.request.urlopen(url).read().decode('utf-8')
+    try:
+        return urllib.request.urlopen(url).read().decode('utf-8')
+    except urllib.request.HTTPError as error:
+        return error.read()
 
-def get_track_count(user, limit='50', period='7days'):
-    print(user)
+def get_track_count(user, limit='500', period='7days'):
     res = []
     xml = make_request({
         'method' : 'user.getTopArtists',
@@ -22,13 +32,15 @@ def get_track_count(user, limit='50', period='7days'):
         'period' : period,
         'api_key' : API_KEY
     })
-    root = ET.fromstring(xml)
-    for i in root.iterfind('./topartists/artist'): 
-        #artist = list(list(i)[6])[0].text
-        artist = list(i)[0].text
-        playcount = list(i)[1].text
-        res.append([artist, playcount])
-    return res
+    error = errors(xml)
+    if not error:
+        root = ET.fromstring(xml)
+        for i in root.iterfind('./topartists/artist'): 
+            artist = list(i)[0].text
+            playcount = list(i)[1].text
+            res.append([artist, playcount])
+        return res
+    raise LastFMError(error)    
 
 def get_track_genre(tracks):
     res = {}
@@ -63,4 +75,9 @@ def to_json(dict_):
     return res
 
 if __name__ == '__main__':
-    print(get_track_genre(get_track_count('username')))
+    try:
+        count = get_track_count('username')
+    except LastFMError as e:
+        print(e)
+    else:
+        print(get_track_genre(count))  
