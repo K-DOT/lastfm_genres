1e8335c5308544b442e506b27f5ad452f59e7266
K-DOT <aleximeky@gmail.com>
2016-01-08 17:57:16 +0400

    Redis on heroku

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-CELERY_BROKER_URL = BROKER_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-CELERY_RESULT_BACKEND = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+CELERY_BROKER_URL = BROKER_URL = os.getenv('HEROKU_REDIS_ROSE_URL', 'redis://localhost:6379')
+CELERY_RESULT_BACKEND = os.getenv('HEROKU_REDIS_ROSE_URL', 'redis://localhost:6379')
